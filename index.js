9905619ceb7224862df41f91a99120210601aa80
Saksham Sharma <saksham@go-gaga-app.com>
2020-12-10 03:15:13 +0530

    small change

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ request.post(options, (err, resp, body) => {
     console.log(`Status: ${resp.statusCode}`);
     console.log("body",JSON.parse(body));
     // console.log('origin',req)
-    res.redirect(`http://localhost:3000/?token=${JSON.parse(body).access_token}`)
+    res.redirect(`https://foreignresearchportal.herokuapp.com/?token=${JSON.parse(body).access_token}`)
 });
 
 })
