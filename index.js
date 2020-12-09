788ec319bf82d0d00e6f779fd612e87bc288a85a
Saksham Sharma <saksham@go-gaga-app.com>
2020-12-09 21:21:56 +0530

    small change

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/',(req,res)=>{
       'content-type': 'application/x-www-form-urlencoded',
       'cache-control': "no-cache",
     },
-    body: `grant_type=authorization_code&client_secret=KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm&client_id=KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI&redirect_uri=https://foreignresearchportal.herokuapp.com&code=${req.query.code}`
+    body: `grant_type=authorization_code&client_secret=KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm&client_id=KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI&redirect_uri=https://frp-backend.herokuapp.com/&code=${req.query.code}`
 };
 
 request.post(options, (err, res, body) => {
@@ -53,6 +53,7 @@ request.post(options, (err, res, body) => {
     }
     console.log(`Status: ${res.statusCode}`);
     console.log("body",body);
+    res.send("Redirecting . hold on for a second")
 });
 
 // payload = "grant_type=authorization_code&client_secret=KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm&client_id=KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI&redirect_uri=https://foreignresearchportal.herokuapp.com/#/&code="+req.query.code
@@ -67,7 +68,7 @@ request.post(options, (err, res, body) => {
 //    console.log(response.body)
 
 //-----------------------------------------------
-  res.send("Redirecting . hold on for a second")
+
 })
 
 app.get('/projects',(req,res)=>{
