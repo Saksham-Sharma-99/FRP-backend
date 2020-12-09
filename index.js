a1b52d88e280041e8d5019e90d95c733c09e5d64
Saksham Sharma <saksham@go-gaga-app.com>
2020-12-09 19:13:22 +0530

    small change

--- index.js
+++ index.js
@@ -14,22 +14,22 @@ app.use(bodyParser.urlencoded({
 
 app.get('/',(req,res)=>{
   console.log(req.query.code)
-  // var params = new URLSearchParams();
-  // params.append('client_id', "KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI");
-  // params.append('client_secret', "KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm");
-  // params.append('grant_type',"authorization_code")
-  // params.append('redirect_uri',"http://localhost:3000/")
-  // params.append('code',req.query.code)
-  // console.log(params)
-  axios.post("https://internet.channeli.in/open_auth/token/", {
+  var params = new URLSearchParams();
+  params.append('client_id', "KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI");
+  params.append('client_secret', "KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm");
+  params.append('grant_type',"authorization_code")
+  params.append('redirect_uri',"http://localhost:3000/")
+  params.append('code',req.query.code)
+  console.log(params)
+  axios.post("https://internet.channeli.in/open_auth/token/", params
     // params :{
-    client_id:"KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI",
-   client_secret:"KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm",
-   grant_type:"authorization_code",
-   redirect_uri:"https://foreignresearchportal.herokuapp.com",
-   code:req.query.code
+   //  client_id:"KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI",
+   // client_secret:"KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm",
+   // grant_type:"authorization_code",
+   // redirect_uri:"https://foreignresearchportal.herokuapp.com",
+   // code:req.query.code
  // }
-  }).then((res) => {
+  ).then((res) => {
     console.log(`statusCode: ${res.statusCode}`)
     console.log(res.data)
   }).catch((error) => {
