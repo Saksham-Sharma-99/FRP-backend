21ca6934df1ded359a50e2453e114b3299bf9069
Saksham Sharma <saksham@go-gaga-app.com>
2020-12-13 00:57:14 +0530

    change to make functioning better

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/',(req,res)=>{
       'content-type': 'application/x-www-form-urlencoded',
       'cache-control': "no-cache",
     },
-    body: `grant_type=authorization_code&client_secret=KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm&client_id=KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI&redirect_uri=https://frp-backend.herokuapp.com/&code=${req.query.code}`
+    body: `grant_type=authorization_code&client_secret=KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm&client_id=KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI&redirect_uri=${req.query.state}&code=${req.query.code}`
 };
 
 request.post(options, (err, resp, body) => {
