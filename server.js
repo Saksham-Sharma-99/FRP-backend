6685464d4020069fb75556e5a4119a68fb4c6e2b
Saksham-Sharma-99 <sakshamsharma99@outlook.com>
2020-12-19 21:03:01 +0530

    changes for aws

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 app.get('/',(req,res)=>{
   console.log(req.query.code)
 
-  var redirect_uri = (req.query.state == "http://localhost:3000/") ? "https://frp-backend.herokuapp.com/" : "http://ec2-13-235-76-138.ap-south-1.compute.amazonaws.com/api/"
+  var redirect_uri = (req.query.state == "http:localhost:3000/#/") ? "https://frp-backend.herokuapp.com/" : "http://ec2-13-235-76-138.ap-south-1.compute.amazonaws.com/api/"
 
   const options = {
     url: 'https://internet.channeli.in/open_auth/token/',
