21a181980a0ea3c75a05186aa3955c9bcb06458e
Saksham-Sharma-99 <sakshamsharma99@outlook.com>
2020-12-21 19:29:53 +0530

    email testing

--- Constants.js
+++ Constants.js
@@ -1,6 +1,10 @@
 const Constants = {
     SECRET : "KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm",
-    CLIENT_ID : 'KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI'
+    CLIENT_ID : 'KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI',
+    EMAIL : "sakshamsharma99@outlook.com",
+    PASS : "Saksham99@iit",
+    SERVICE : "outlook",
+    
 }
 
 module.exports = {
---
+++ Moderator/EmailsManager.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer")
+const fs = require("fs")
+const Constant = require("../Constants")
+const Constants = Constant.Constants
+const transporter = nodemailer.createTransport(
+    {
+        service : Constants.SERVICE,
+        auth :{
+            user:Constants.EMAIL,
+            pass: Constants.PASS
+        }
+    }
+)
+
+function sendMail(userId,postId){
+    let rawUserData = fs.readFileSync(__dirname+'../database/data/users/users.json')
+    let users = JSON.parse(rawUserData)
+    let rawPostData = fs.readFileSync(__dirname+"../database/demoData/projects.json")
+    let projects = JSON.parse(rawPostData)
+    var mailOptions = {
+        from: Constants.EMAIL,
+        to: users.users.filter((user)=>user.userId == userId)[0].contactInformation.instituteWebmailAddress,
+        subject: 'Updates on your application',
+        text: `Your Application for semester exchange with ${projects.projects.filter((project)=>project.postId == postId)[0].data.name} was successful`
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
