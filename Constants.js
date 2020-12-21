9c09823d2f1a8fb6c9e6ade655f33b3e5950eab7
Saksham-Sharma-99 <sakshamsharma99@outlook.com>
2020-12-21 19:52:12 +0530

    email testing

--- Constants.js
+++ Constants.js
@@ -2,9 +2,9 @@ const Constants = {
     SECRET : "KiSTNolWFrQEehYloliUyLRdauKG2XczUL0ST4HapeZXA68XnaOMZ7nWLg6SAwtbJxG7UWlnXdyVO9Do0rcaqFKFxT86ZVmJ5jDRtstmi5Wzidrlk9fh5oZa6CyGegUm",
     CLIENT_ID : 'KhvKozOsGjVXmRNZcvL8SB8S9XxZ7PKJOfazP9sI',
     EMAIL : "sakshamsharma99@outlook.com",
-    PASS : "Saksham99@iit",
+    PASS : "shutthefuckup123",
     SERVICE : "outlook",
-    
+
 }
 
 module.exports = {
