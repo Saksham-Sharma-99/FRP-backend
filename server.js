e5103232a5f84077643bd2a4dcd868b5597f5ff4
Saksham-Sharma-99 <sakshamsharma99@outlook.com>
2020-12-27 01:41:22 +0530

    small change

--- server.js
+++ server.js
@@ -76,12 +76,9 @@ app.get(Constants.Routes.checkUser,(req,res)=>{
 })
 app.get(Constants.Routes.files ,(req,res)=>{
   console.log(req.query.name)
-  var file = fs.createReadStream(__dirname+'/public/files/'+req.query.name);
-  var stat = fs.statSync(__dirname+'/public/files/'+req.query.name);
-  res.setHeader('Content-Length', stat.size);
-  res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
-  file.pipe(res);
+  var data =fs.readFileSync(__dirname+'/public/files/'+req.query.name);
+  res.contentType("application/pdf");
+  res.send(data);
 })
 
 
